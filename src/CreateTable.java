4edb9c72c4ec3b711f476cb6cb6dfaced25aeec7
Henrique <henrique1258@gmail.com>
2020-12-09 15:31:53 -0300

    Concatena a saída da classe da RFC em campos para o Talend

---
+++ src/CreateTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CreateTable {
+
+    public static void main(String[] args){
+
+        Scanner rfcColumns = new Scanner(System.in);  //Create a Scanner object
+        System.out.print("Enter table columns: ");
+        String readColumns = rfcColumns.nextLine();  //Read user input
+        String columnToBeSplitted = readColumns;
+        String[] splitParts = columnToBeSplitted.split(";");
+        System.out.println("Paste the next lines into Talend Open Studio tJavaFlex (main):");
+        for (int i = 0; i < splitParts.length; i++){
+            String actualColumn = splitParts[i];
+            System.out.println("row1." + actualColumn + " = table.getString(\"" + actualColumn +"\");");
+        }
+    }
+}
